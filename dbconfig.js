933f46de23ddc9d556f545ff169bb6608efd7ed6
chonlasak <chonlasak@rmutl.ac.th>
2024-03-08 10:21:12 +0700

    adduser

--- dbconfig.js
+++ dbconfig.js
@@ -5,15 +5,15 @@ var dbconfig = {
         port: '3306',
         user: 'root',
         password: '',
-        database: 'term_project_db'
+        database: 'term_project'
     },
     production: {
         //connectionLimit : 10,
-        host: 'localhost',
-        port: '3306',
-        user: 'dbuser66',
-        password: 'P@ssw0rd',
-        database: 'user66008_db'
+        host: "localhost",
+        port: "3306",
+        user: "dbuser66",
+        password: "P@ssw0rd",
+        database: "user66008_db",
     }
 };
 module.exports = dbconfig;
